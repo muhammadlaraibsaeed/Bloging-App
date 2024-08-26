8b2a7d6abf8c788e6830e65b98b727b4463a928b
muhammadlaraibsaeed <muhammadlaraibsaeed@gmail.com>
2024-08-26 05:37:50 -0700

    initials migration

---
+++ app/Repositories/PostRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repositories;
+use App\Contracts\RepositoryInterface;
+
+class PostRepository implements RepositoryInterface
+{
+    public function all(){
+
+    }
+
+    public function find($id){
+
+    }
+
+    public function create(array $data){
+
+    }
+
+    public function update($id, array $data){
+
+    }
+
+    public function delete($id){
+        
+    }
+}
