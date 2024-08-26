50983679074fffe2482db95438e1f3b150949b18
muhammadlaraibsaeed <muhammadlaraibsaeed@gmail.com>
2024-08-26 05:38:22 -0700

    commit

--- database/migrations/2024_08_26_122859_create_posts_table.php
+++ database/migrations/2024_08_26_122859_create_posts_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
